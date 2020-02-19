91265bfe29c1ffe1c4d5ed90df64526b2a4b0119
simon <zhuxinming8@163.com>
2020-02-19 22:03:15 +0800

    优化订单代码

--- YiShangbao/Feature/SellerTabController/OrderManagement/Views/OrderList/OrderCellFooterView.h
+++ YiShangbao/Feature/SellerTabController/OrderManagement/Views/OrderList/OrderCellFooterView.h
@@ -4,7 +4,8 @@
 //
 //  Created by simon on 2017/9/8.
 //  Copyright © 2017年 com.Microants. All rights reserved.
-//  订单管理列表组尾
+//  订单管理列表组尾= 1.更多按钮展示容器+2.价格展示容器+3.动态个数操作按钮容器
+//  动态展示高度，根据字段是否展示更多按钮容器view；
 
 #import <UIKit/UIKit.h>
 #import "SetDataProtocol.h"
@@ -15,12 +16,13 @@
 
 @interface OrderCellFooterView : UITableViewHeaderFooterView<SetDataProtoct>
 
+///1.更多按钮展示容器
 @property (weak, nonatomic) IBOutlet UIView *moreContainerView;
 
 @property (weak, nonatomic) IBOutlet UIButton *moreBtn;
 
 
-
+///2.价格展示容器
 @property (weak, nonatomic) IBOutlet UIView *totalPriceContainerView;
 
 //（含运费Y21.00）
@@ -31,7 +33,7 @@
 //几件商品描述
 @property (weak, nonatomic) IBOutlet UILabel *numProDesLab;
 
-//外部容器
+///3.动态个数操作按钮容器
 @property (weak, nonatomic) IBOutlet UIView *labelTagsContainerView;
 
 @property (weak, nonatomic) IBOutlet ZXLabelsTagsView *labelsTagsView;
